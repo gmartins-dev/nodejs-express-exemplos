acf129c068e764a907165cc6ee938cb3860e8b37
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-22 07:53:51 -0300

    webpack instalado

---
+++ README.md
@@ -0,0 +1,15 @@
+.md = mark down
+
+# Passos para rodar o projeto: 
+
+- Instalar todas dependencias com `npm i`
+
+- Instalar o webpack globalmente com `npm i -g webpack@5.68.0`
+
+- Instalar demais dependencias `npm i webpack@5.68.0 webpack-cli@4.9.2 nodemon-webpack-plugin@4.7.0 webpack-node-externals@3.0.0 --save-dev`
+
+- Rodar localmente usando `npm run dev`
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const NodemonPLugin = require('nodemon-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+    entry: './src/index.js',
+    target: 'node',
+    mode: 'development',
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extension: ['.js']
+    },
+    plugins: [
+        new NodemonPLugin()
+    ],
+    externals: [
+        nodeExternals()
+    ]
+}
