71b87ba0ee3cfa56fbab138bb270910516568ff0
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-22 07:58:07 -0300

    webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
-        extension: ['.js']
+        extensions: ['.js']
     },
     plugins: [
         new NodemonPLugin()
